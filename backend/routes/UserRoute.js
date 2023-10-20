254bb7cc56ac77af7bbcb5c3d710dce012499dd1
haikaljs <haikaljavascript@gmail.com>
2023-10-20 22:15:59 +0800

    create user

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { getUsers, getUserById } from "../controllers/UserController.js";
+import {
+  getUsers,
+  getUserById,
+  createUser,
+} from "../controllers/UserController.js";
 
 const router = express.Router();
 
 router.get("/users", getUsers);
 router.get("/users/:id", getUserById);
+router.post("/users", createUser);
 
 export default router;
